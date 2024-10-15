d97304261fe6cd3b31fe8858aaec7eaf2e562473
evg3noff <evgenkleninoff@gmail.com>
2024-10-15 20:44:26 +0400

    delete all files on main

--- src/components/CustomButtons.js
+++
@@ -1,9 +0,0 @@
-import styles from "./customButtons.module.css"; 
-import React, { useState } from 'react';
-
-const CustomButtons = ({text, onActive, id, onButtonClick, count, BtnId}) => {
-
-        return  <div id={id} onClick={onButtonClick} className ={onActive ? styles.CustomButtons_menu_option_active : styles.CustomButtons_menu_option}>{text}</div>;
-    }
-
-export default CustomButtons;
